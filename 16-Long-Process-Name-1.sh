2c3f244b10171b728ec3c60c101e670875905e32
Matthew Malensek <matt@malensek.net>
2019-09-02 23:59:42 -0700

    Switch -t flag

--- 16-Long-Process-Name-1.sh
+++ 16-Long-Process-Name-1.sh
@@ -2,7 +2,7 @@ source "${TEST_DIR}/funcs.bash"
 
 test_start "Correct Process Name Truncation"
 
-./inspector -p "${TEST_DIR}/fakeproc" -l \
+./inspector -p "${TEST_DIR}/fakeproc" -t \
     | grep '88888' | sed 's/[[:space:]]//g' \
     | grep '|really_really_long_named_|'
 
