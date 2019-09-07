9d07517dfc36ead5939a9bb84ec770995a8204b4
Matthew Malensek <matt@malensek.net>
2019-09-07 00:44:13 -0700

    Add comment explaining checks

--- 01-All-Headers-1.sh
+++ 01-All-Headers-1.sh
@@ -1,9 +1,14 @@
 source "${TEST_DIR}/funcs.bash"
 
 test_start "System/Hardware/Task Headers"
+
+# This checks that each of the required headers is present. For the task list,
+# each column heading is checked (note that spaces are ignored).
+
 output_text=$(./inspector -a)
 grep -q 'System Information' <<< "${output_text}" \
     && grep -q 'Hardware Information' <<< "${output_text}" \
     && grep -q 'Task Information' <<< "${output_text}" \
-    && sed 's/[[:space:]]//g' <<< "${output_text}" | grep -q 'PID|State|TaskName|User|Tasks'
+    && sed 's/[[:space:]]//g' <<< "${output_text}" \
+        | grep -q 'PID|State|TaskName|User|Tasks'
 test_end
