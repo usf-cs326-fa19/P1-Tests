5e1239fe8d6a405068d30ac32c61da564318bfc1
Matthew Malensek <matt@malensek.net>
2019-09-14 00:07:45 -0700

    Add -Wall to compiler warning check

--- 22-Compiler-Warnings-1.sh
+++ 22-Compiler-Warnings-1.sh
@@ -3,6 +3,6 @@ source "${TEST_DIR}/funcs.bash"
 test_start "Compiler warnings check" \
     "To pass, eliminate all compiler warnings/errors"
 
-gcc "${TEST_DIR}"/../*.c
+gcc -Wall -lm "${TEST_DIR}"/../*.c
 
 test_end
