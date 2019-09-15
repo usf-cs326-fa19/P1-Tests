b539691d2d9f26faee14509e0b166d86974dfacc
Matthew Malensek <matt@malensek.net>
2019-09-14 23:36:04 -0700

    More robust header check

--- 01-All-Headers-1.sh
+++ 01-All-Headers-1.sh
@@ -1,14 +1,45 @@
 source "${TEST_DIR}/funcs.bash"
 
-test_start "System/Hardware/Task Headers"
+expected_headings1="Hardware Information
+System Information
+Task Information"
 
-# This checks that each of the required headers is present. For the task list,
-# each column heading is checked (note that spaces are ignored).
+expected_headings2="Hardware Information"
 
-output_text=$(./inspector -a)
-grep -q 'System Information' <<< "${output_text}" \
-    && grep -q 'Hardware Information' <<< "${output_text}" \
-    && grep -q 'Task Information' <<< "${output_text}" \
-    && sed 's/[[:space:]]//g' <<< "${output_text}" \
-        | grep -q 'PID|State|TaskName|User|Tasks'
+expected_headings3="System Information"
+
+expected_headings4="Task Information"
+
+expected_headings5="System Information
+Task Information"
+
+test_start "System/Hardware/Task Headers" \
+    "Tests that the various heading combinations are present."
+
+reported_headings=$(./inspector -a \
+    | grep -E 'System Information|Hardware Information|Task Information' | sort)
+compare --ignore-all-space \
+    <(echo "${expected_headings1}") <(echo "${reported_headings}") || test_end 1
+
+reported_headings=$(./inspector -r \
+    | grep -E 'System Information|Hardware Information|Task Information' | sort)
+compare --ignore-all-space \
+    <(echo "${expected_headings2}") <(echo "${reported_headings}") || test_end 1
+
+reported_headings=$(./inspector -s \
+    | grep -E 'System Information|Hardware Information|Task Information' | sort)
+compare --ignore-all-space \
+    <(echo "${expected_headings3}") <(echo "${reported_headings}") || test_end 1
+
+reported_headings=$(./inspector -t \
+    | grep -E 'System Information|Hardware Information|Task Information' | sort)
+compare --ignore-all-space \
+    <(echo "${expected_headings4}") <(echo "${reported_headings}") || test_end 1
+
+reported_headings=$(./inspector -ts \
+    | grep -E 'System Information|Hardware Information|Task Information' | sort)
+compare --ignore-all-space \
+    <(echo "${expected_headings5}") <(echo "${reported_headings}") || test_end 1
+
+
 test_end
