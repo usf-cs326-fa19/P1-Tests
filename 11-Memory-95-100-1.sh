0788a4cea35cfc05a6c5a9a496615bfe51382014
Matthew Malensek <matt@malensek.net>
2019-09-14 23:57:17 -0700

    Fix 95/100 memory test

--- 11-Memory-95-100-1.sh
+++ 11-Memory-95-100-1.sh
@@ -1,20 +1,23 @@
 source "${TEST_DIR}/funcs.bash"
 
+expected_usage1="Memory Usage: [###################-] 94.9% (474.5 GB / 500.0 GB)"
+expected_usage2="Memory Usage: [####################] 100.0% (500.0 GB / 500.0 GB)"
+
 test_start "Memory: 94.9%, 100% Usage" \
     "Checks the output and progress bar when memory usage is 94.9% and 100%."
 
 cp "${TEST_DIR}/mem/95" "${TEST_DIR}/fakeproc/meminfo"
-./inspector -p "${TEST_DIR}/fakeproc" -r | sed 's/[[:space:]]//g' \
-    | grep -i 'MemoryUsage:\[##################--\]94.9%(4....GB/5....GB)'
-result1=${?}
+reported_usage1=$(./inspector -p "${TEST_DIR}/fakeproc" -r \
+    | grep -i 'Memory[[:space:]]*Usage:')
+cp "${TEST_DIR}/mem/original" "${TEST_DIR}/fakeproc/meminfo"
+compare --ignore-all-space \
+    <(echo "${expected_usage1}") <(echo "${reported_usage1}") || test_end 1
 
 cp "${TEST_DIR}/mem/100" "${TEST_DIR}/fakeproc/meminfo"
-# Spaces are removed before doing the string comparison:
-./inspector -p "${TEST_DIR}/fakeproc" -r | sed 's/[[:space:]]//g' \
-    | grep -i 'MemoryUsage:\[####################\]100.0%(5....GB/5....GB)'
-result2=${?}
-
-
+reported_usage2=$(./inspector -p "${TEST_DIR}/fakeproc" -r \
+    | grep -i 'Memory[[:space:]]*Usage:')
 cp "${TEST_DIR}/mem/original" "${TEST_DIR}/fakeproc/meminfo"
-[ ${result1} -eq 0 ] && [ ${result2} -eq 0 ]
+compare --ignore-all-space \
+    <(echo "${expected_usage2}") <(echo "${reported_usage2}") || test_end 1
+
 test_end
