6cc18ad73e36ea89a0ab9ae62a957a66163de03d
Matthew Malensek <matt@malensek.net>
2019-09-14 23:06:08 -0700

    Use compare for hostname check

--- 02-Hostname-1.sh
+++ 02-Hostname-1.sh
@@ -4,14 +4,11 @@ test_start "Hostname Check"
 
 expected_hostname="Hostname: deus-ex-machina"
 actual_hostname=$(./inspector -p "${TEST_DIR}/fakeproc" -s | grep 'Hostname')
-
 compare --ignore-all-space \
     <(echo "${expected_hostname}") <(echo "${actual_hostname}") || test_end 1
 
-
 expected_hostname="Hostname: $(hostname)"
 actual_hostname=$(./inspector -s | grep 'Hostname')
-
 compare --ignore-all-space \
     <(echo "${expected_hostname}") <(echo "${actual_hostname}") || test_end 1
 
