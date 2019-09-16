6a6998c489f7ee135af5e28935c5169f8cdeffbd
Matthew Malensek <matt@malensek.net>
2019-09-16 16:21:52 -0700

    Test 17 folded into 16

--- 17-Task-Count-1.sh
+++
@@ -1,9 +0,0 @@
-source "${TEST_DIR}/funcs.bash"
-
-test_start "Correct task list contents and sub-task count (9256)"
-
-./inspector -p "${TEST_DIR}/fakeproc" -t \
-    | grep '88888' | sed 's/[[:space:]]//g' \
-    | grep '9256$'
-
-test_end
