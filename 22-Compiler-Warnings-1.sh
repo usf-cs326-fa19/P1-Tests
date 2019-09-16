3dc91eab3363d5d6fa68d8fbb9f9fe2b874692b7
Matthew Malensek <matt@malensek.net>
2019-09-16 10:27:49 -0700

    Fix issue with test always passing

--- 22-Compiler-Warnings-1.sh
+++ 22-Compiler-Warnings-1.sh
@@ -3,6 +3,6 @@ source "${TEST_DIR}/funcs.bash"
 test_start "Compiler warnings check" \
     "To pass, eliminate all compiler warnings/errors"
 
-gcc -Wall -lm "${TEST_DIR}"/../*.c
+gcc -Wall -Werror -lm "${TEST_DIR}"/../*.c
 
 test_end
