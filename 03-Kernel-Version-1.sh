b4ef8cac5b36a40a66c02ce22a845dbb7238efd6
Matthew Malensek <matt@malensek.net>
2019-09-16 16:28:48 -0700

    Ignore case

--- 03-Kernel-Version-1.sh
+++ 03-Kernel-Version-1.sh
@@ -4,12 +4,12 @@ test_start "Kernel Version"
 
 expected_version="Kernel Version: 1.33.7-111.1.1.el7.x86_64"
 reported_version=$(./inspector -p "${TEST_DIR}/fakeproc" -s | grep -i 'kernel')
-compare --ignore-all-space \
+compare --ignore-all-space --ignore-case \
     <(echo "${expected_version}") <(echo "${reported_version}") || test_end 1
 
 expected_version="Kernel Version: $(uname -r)"
 reported_version=$(./inspector -s | grep -i 'Kernel')
-compare --ignore-all-space \
+compare --ignore-all-space --ignore-case \
     <(echo "${expected_version}") <(echo "${reported_version}") || test_end 1
 
 test_end
