822f5d201dd95a826ed58cdd34915eecdfda26ca
Matthew Malensek <matt@malensek.net>
2019-09-26 11:44:42 -0700

    Fix uptime basic issue

--- 04-Uptime-Basic-1.sh
+++ 04-Uptime-Basic-1.sh
@@ -1,5 +1,6 @@
 source "${TEST_DIR}/funcs.bash"
 
+cp "${TEST_DIR}/orig_uptime" "${TEST_DIR}/fakeproc/uptime"
 expected_uptime="Uptime: 17 days, 5 hours, 38 minutes, 55 seconds"
 
 test_start "Basic Uptime Formatting"
